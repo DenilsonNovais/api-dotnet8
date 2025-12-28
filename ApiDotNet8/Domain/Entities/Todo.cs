b337bd96a0a86aad9b898ffdf62c619964fd2631
Denilson Santos <denilson_n.santos@hotmail.com>
2025-12-27 23:24:36 -0300

    feat: add Todo domain entity

---
+++ ApiDotNet8/Domain/Entities/Todo.cs
@@ -0,0 +1,21 @@
+using System.Net.NetworkInformation;
+
+namespace ApiDotNet8.Domain.Entities;
+
+public class Todo
+{
+    public Guid Id { get; private set;}
+    public string? Title { get; private set;}
+    public bool Completed { get; private set;}
+
+    protected Todo() {}
+
+    public Todo(string title)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Completed = false;
+    }
+
+    public void Complete() => Completed = true;
+}
